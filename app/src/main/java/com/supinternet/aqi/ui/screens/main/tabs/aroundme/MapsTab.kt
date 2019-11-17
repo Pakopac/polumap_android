8a4b976ced2d47cc713b54ff2eac5e0ddf29493e
Pakopac <lilian.pacaud@supinternet>
2019-11-17 17:07:14 +0100

    Add Markers on search

--- app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/aroundme/MapsTab.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/aroundme/MapsTab.kt
@@ -1,5 +1,8 @@
 package com.supinternet.aqi.ui.screens.main.tabs.aroundme
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,6 +14,7 @@ import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
 import com.google.android.material.button.MaterialButton
 import com.supinternet.aqi.R
+import com.supinternet.aqi.ui.utils.GoogleMapUtils
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Retrofit
@@ -43,8 +47,8 @@ class MapsTab : Fragment(), OnMapReadyCallback {
         val data: List<GetData>
     )
     data class GetData(
-        val lat: Float,
-        val lon: Float,
+        val lat: Double,
+        val lon: Double,
         val uid: Float,
         val aqi: String,
         val station: Map<String, String>?
@@ -79,12 +83,48 @@ class MapsTab : Fragment(), OnMapReadyCallback {
         val btn_search = view!!.findViewById(R.id.maps_tab_search_zone_button) as MaterialButton
 
         btn_search.setOnClickListener {
-            getAPI("39.379436,116.091230,40.235643,116.784382", object : retrofit2.Callback<Response> {
+            val coordinate:String =
+                map.getProjection().getVisibleRegion().farLeft.latitude.toString() + ',' +
+                        map.getProjection().getVisibleRegion().farLeft.longitude.toString() + ',' +
+                        map.getProjection().getVisibleRegion().nearRight.latitude.toString() + ',' +
+                        map.getProjection().getVisibleRegion().nearRight.longitude.toString()
+
+            getAPI(coordinate, object : retrofit2.Callback<Response> {
 
                 override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+                    map.clear()
+                    var count = 0
                     val GetDatas = response.body()
-                    Log.v("aaaaa", response.toString())
-                    Log.v("abcde", GetDatas.toString())
+                    Log.v("aaaaa", response.body().toString())
+                    if(response.body()?.data.isNullOrEmpty()){
+                        val dialogBuilder = AlertDialog.Builder(view?.context)
+                        dialogBuilder.setTitle("Aucune station")
+
+                            .setMessage("Aucune station n'existe. Veuillez essayer d'élargir la zone de recheche")
+                            .setNegativeButton("OK", DialogInterface.OnClickListener {
+                                    dialog, id -> dialog.cancel()
+                            })
+                            .create()
+                            .show()
+                    }
+                    else{
+                        for(item in response.body()!!.data){
+                            if(count < 20) {
+                                count++
+                                map.addMarker(MarkerOptions()
+                                    .position(LatLng(item.lat, item.lon))
+                                    .title(item.station?.get("name"))
+                                    .icon(BitmapDescriptorFactory.fromBitmap(
+                                        GoogleMapUtils.getBitmap(
+                                            requireContext(),
+                                            R.drawable.ic_map_marker)))
+                                )
+                            }
+                        }
+                    }
+                    //map.addMarker(MarkerOptions().position(LatLng(47.055817, 1.042124 )).title("Verneuil, France"))
+
+                    //map.addMarker(MarkerOptions().position(LatLng(44.92545, 1.8457342)).title("Marker1"))
                 }
 
                 override fun onFailure(call: Call<Response>, t: Throwable) {
