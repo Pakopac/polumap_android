d2f99ac09630594fb6f0a2c83a80650ab07dba58
Pakopac <lilian.pacaud@supinternet>
2019-11-17 15:21:31 +0100

    Get datas getMapBounds onclick

--- app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/aroundme/MapsTab.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/aroundme/MapsTab.kt
@@ -1,13 +1,22 @@
 package com.supinternet.aqi.ui.screens.main.tabs.aroundme
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
+import com.google.android.material.button.MaterialButton
 import com.supinternet.aqi.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 class MapsTab : Fragment(), OnMapReadyCallback {
@@ -30,6 +39,34 @@ class MapsTab : Fragment(), OnMapReadyCallback {
         )
     }
 
+    data class Response(
+        val data: List<GetData>
+    )
+    data class GetData(
+        val lat: Float,
+        val lon: Float,
+        val uid: Float,
+        val aqi: String,
+        val station: Map<String, String>?
+    )
+
+    interface API {
+        @GET("getMapBounds")
+
+        fun getDataById(@Query("latlng") id:String) : Call<Response>
+    }
+
+    val api = Retrofit.Builder()
+        .baseUrl("https://us-central1-polumapproject.cloudfunctions.net/api/")
+        .addConverterFactory(
+            GsonConverterFactory.create())
+        .build()
+        .create(API::class.java)
+
+    fun getAPI(latlng: String, callback: Callback<Response>) {
+        return api.getDataById(latlng).enqueue(callback)
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
@@ -38,6 +75,25 @@ class MapsTab : Fragment(), OnMapReadyCallback {
                 requireContext(), R.raw.map_style
             )
         )
+
+        val btn_search = view!!.findViewById(R.id.maps_tab_search_zone_button) as MaterialButton
+
+        btn_search.setOnClickListener {
+            getAPI("39.379436,116.091230,40.235643,116.784382", object : retrofit2.Callback<Response> {
+
+                override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+                    val GetDatas = response.body()
+                    Log.v("aaaaa", response.toString())
+                    Log.v("abcde", GetDatas.toString())
+                }
+
+                override fun onFailure(call: Call<Response>, t: Throwable) {
+
+                    Log.v("abcde", t.toString())
+                    t.printStackTrace();
+                }
+            });
+        }
     }
 
 }
